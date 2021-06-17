8495b2d6f1fcb82d8913d0945b29f3a14e47c9bc
andrew66882011 <40665773+andrew66882011@users.noreply.github.com>
2021-06-16 23:07:29 -0400

    update chromedrive

--- glassdoor_scraper.py
+++ glassdoor_scraper.py
@@ -22,7 +22,7 @@ def get_jobs(keyword, num_jobs, verbose, path, slp_time):
     #options.add_argument('headless')
     
     #Change the path to where chromedriver is in your home folder.
-    driver = webdriver.Chrome(executable_path=-path, options=options)
+    driver = webdriver.Chrome(executable_path=path, options=options)
     driver.set_window_size(1120, 1000)
 
     url = 'https://www.glassdoor.com/Job/jobs.htm?sc.keyword="' + keyword + '"&locT=C&locId=1147401&locKeyword=San%20Francisco,%20CA&jobType=all&fromAge=-1&minSalary=0&includeNoSalaryJobs=true&radius=100&cityId=-1&minRating=0.0&industryId=-1&sgocId=-1&seniorityType=all&companyId=-1&employerSizes=0&applicationType=0&remoteWorkType=0'
--- main.py
+++ main.py
@@ -6,9 +6,9 @@ Created on Wed Jun 16 14:57:28 2021
 @author: jjhua
 """
 
-import glassdoor_scrapper as gs
+import glassdoor_scraper as gs
 import pandas as pd
 
-path = "/Users/jjhua/Desktop/Python for Data Science/ds_salary_proj/"
+path = "/Users/jjhua/Desktop/Python for Data Science/ds_salary_proj/chromedriver"
 
 df = gs.get_jobs("data scientist", 15, False, path, 15)
