d5614886e312a23af3b3f7a626927cb56e69b782
andrew66882011 <40665773+andrew66882011@users.noreply.github.com>
2021-06-16 22:12:16 -0400

    second commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 14:57:28 2021
+
+@author: jjhua
+"""
+
+import glassdoor_scrapper as gs
+import pandas as pd
+
+path = "/Users/jjhua/Desktop/Python for Data Science/ds_salary_proj/"
+
+df = gs.get_jobs("data scientist", 15, False, path, 15)
